6e1d538fd53e809f4e7929e8dc11f41256fdf9c8
Marc <valente.marc@gmail.com>
2014-05-10 14:53:01 -0700

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bookmarcs::Application.config.secret_key_base = 'e3280ae8df010df56f58857fbcd4a4f74ed98ce38796025c69cec7d6a77f5e92f428bc2ffd9ed10b6bb843315dd34911164ba86e4cceba776158de16018c0d81'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Bookmarcs::Application.config.session_store :cookie_store, key: '_bookmarcs_session'
