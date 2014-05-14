7f42997485fc1417ee8945a82ac31db9fd80b335
Marc <valente.marc@gmail.com>
2014-05-14 15:58:34 -0700

    Addid sing up and some sing in functionality

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,5 +29,6 @@ Bookmarcs::Application.configure do
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+# Create Users
+5.times do
+  user = User.new(
+    name:     Faker::Name.name,
+    email:    Faker::Internet.email,
+    password: Faker::Lorem.characters(10)
+  )
+  user.skip_confirmation!
+  user.save
+end
+
+users = User.all
+
+User.first.update_attributes(
+  email: 'valente.marc@gmail.com',
+  password: 'helloworld',
+  )
